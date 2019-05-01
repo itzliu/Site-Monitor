5249b6d7b1ea49b4bea5f17d1dd6934e3e6d60bb
Harry Liu <harryliu1995@gmail.com>
2019-05-01 12:48:11 -0400

    first commit

---
+++ README.md
@@ -0,0 +1,81 @@
+# Site-Monitor
+Python script to monitor websites and email you if the site is down.
+
+## What I Learned
+* How to build a script to monitor website.
+* How to use smtplib to send emails.
+* How to interact with Linode servers through Linode API.
+* Logging Basics
+
+## Getting Started
+These instructions will get you a copy of the project up and running on your local machine for development and testing purposes.
+
+### Prerequisites
+* Python3.7
+* pip3
+
+### Installing
+A step by step set of instructions to get a development environment running.
+
+Setup configuration file for email services and your linode token.
+```
+sudo nano /etc/config.json
+```
+```
+{
+        "MAIL_USERNAME":"email",
+        "MAIL_PASSWORD":"password",
+        "MAIL_DEFAULT_SENDER":"email",
+        "LINODE_TOKEN":"token"
+}
+```
+Clone the repository onto your machine.
+```
+git clone https://github.com/itzliu/Site-Monitor.git
+```
+Change directory into the project directory folder.
+```
+cd Site-Monitor
+```
+Create a virtual environment for the app.
+```
+python3 -m venv monitor_env
+```
+Activate your virtual environment.
+```
+source monitor_env/bin/activate
+```
+Install the Pyhon dependencies.
+```
+pip install -r requirements.txt
+```
+
+To automate this script to check your websites you can do the following.
+
+Find Python environment path, something like '~/Site-Monitor/monitor_env/bin/python'
+which python
+
+Open cron
+```
+crontab -e
+```
+Press 'i' to enter edit mode and add the cron script.
+```
+*/10 * * * * [environement path] [monotor.py path]
+```
+Press 'Esc' to exit edit mode and type in ':wq' to write and quit.
+
+## Running Tests
+N/a
+
+## Built With
+* smtplib
+* requests
+* logging
+* linode_api4
+
+## Authors
+* **Harry Liu**
+
+## Acknowledgements
+* Script built based on [this Flask script](https://github.com/CoreyMSchafer/code_snippets/blob/master/Python/Site-Monitor/monitor.py) from YouTube by [Corey Scafer](https://www.youtube.com/channel/UCCezIgC97PvUuR4_gbFUs5g).
---
+++ monitor.py
@@ -0,0 +1,74 @@
+import os
+import smtplib
+import requests
+import logging
+from linode_api4 import LinodeClient, Instance
+
+# Specify website name, url, and linode-server.
+
+WEBSITES = [
+    {
+        'name': 'iamharryliu',
+        'url': 'https://www.iamharryliu.cosadfm/',
+        'linode-server': 'flask-server'
+    },
+    {
+        'name' : 'Google',
+        'url' : 'https://www.google.ca/', 
+    },
+    {
+        'name' : 'Youtube',
+        'url' : 'https://www.youtube.com/'
+    }
+]
+EMAIL_ADDRESS = os.environ.get('EMAIL_USER')
+EMAIL_PASSWORD = os.environ.get('EMAIL_PASS')
+EMAIL_DEFAULT_SENDER = os.environ.get('EMAIL_DEFAULT_SENDER')
+LINODE_TOKEN = os.environ.get('LINODE_TOKEN')
+
+logging.basicConfig(filename='log.log',
+                    level=logging.INFO,
+                    format='%(asctime)s:%(levelname)s:%(message)s')
+
+def notify_user():
+    with smtplib.SMTP('smtp.gmail.com', 587) as smtp:
+        smtp.ehlo()
+        smtp.starttls()
+        smtp.ehlo()
+        smtp.login(EMAIL_ADDRESS, EMAIL_PASSWORD)
+        subject = f'{website["name"]} SITE IS DOWN!'
+        body = 'Make sure the server restarted and it is back up'
+        msg = f'Subject: {subject}\n\n{body}'
+        logging.info('Sending Email...')
+        smtp.sendmail(EMAIL_ADDRESS, EMAIL_ADDRESS, msg)
+
+def reboot_server():
+    if 'linode-server' in website:
+        reboot_linode_server()
+
+def reboot_linode_server():
+    server_id = get_linode_server_id()
+    my_server = client.load(Instance, server_id)
+    my_server.reboot()
+    logging.info('Attempting to reboot server...')
+
+def get_linode_server_id():
+    client = LinodeClient(LINODE_TOKEN)
+    for linode in client.linode.instances():
+        if linode.label == website['linode-server']:
+            server_id = linode.id
+    return server_id
+
+for website in WEBSITES:
+    try:
+        r = requests.get(website['url'], timeout=5)
+        if r.status_code != 200:
+            logging.info(f'{website["name"]} is DOWN!')
+            notify_user()
+            reboot_server()\
+        else:
+            logging.info(f'{website["name"]} is UP')
+    except Exception as e:
+        logging.info(f'{website["name"]} is DOWN!')
+        notify_user()
+        reboot_server()
